e6780f28f4e60be797c6e458a2a3afbb6510def7
Ben Wright-Jones <bwrightjones@outlook.com>
2023-01-30 10:59:48 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,8 +51,9 @@ streamlit.stop()
 #my_data_rows = my_cur.fetchall()
 
 streamlit.header("The fruit load list contains:")
+
 def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur
+    with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
     return my_cur.fetchall()
 
